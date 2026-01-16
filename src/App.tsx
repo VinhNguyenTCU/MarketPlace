dd089912a976175f87c432883b9b671be16011a8
Khoadoanduy <khoadoanduy05@gmail.com>
2026-01-16 14:29:29 -0600

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://example.com" target="_blank">
+          <img src={viteLogo} className="logo" alt="Example logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -22,15 +19,14 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code> TEST TEST TEST CI </code> asjdfnsafads
-          123 test test
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
